f2d32e255bd192f79b989ac961603da94013e27c
Michal Motor <michalekmotor@gmail.com>
2025-11-09 18:54:27 +0100

    wykonanie zadania 4.4

--- 03-ControlStructures/test.py
+++ 03-ControlStructures/test.py
@@ -338,4 +338,41 @@
 # ------------------------------------------------------------- #
 
 
-# 4
+# 4.1
+
+
+# for i in range(4):
+#     print('Practice makes perfect!')
+
+
+
+# 4.4
+
+
+###
+# Prints the name of university where you are studying
+# with an extra space between characters (add a space between
+# each character), e.g.
+# 'book' -> 'b o o k'
+#
+university = 'Krakow University of Economics'
+university_expanded = ''
+
+for char in university:
+    university_expanded = university_expanded + char + ' '
+    
+    
+    
+print(university)
+print(university_expanded)
+
+# print(university) # original university name
+# print(university_expanded) # expanded university name
+
+
+
+
+
+
+
+
