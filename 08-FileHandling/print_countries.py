8babedd9638556ab0ba294db47c64a31539de29d
Michal Motor <michalekmotor@gmail.com>
2025-12-07 22:11:02 +0100

    skonczone do fileHandling 2.3

--- 08-FileHandling/print_countries.py
+++ 08-FileHandling/print_countries.py
@@ -2,5 +2,7 @@
 # Reads from file, line by line
 #
 with open('countries.txt', 'r') as file:
+    licznik = 1
     for line in file:
-        print(line, end="")
+        print(f"{licznik}.{line}", end="")
+        licznik += 1
