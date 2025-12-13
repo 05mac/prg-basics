cdccceec4b3ecc18bfa249fb15991c36890a7b10
Michal Motor <michalekmotor@gmail.com>
2025-12-13 10:15:01 +0100

    zrobienie book,socialmedia,student,taxi

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,15 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def dislay_timeline(self):
+        print(f'{self.username}, {len(self.posts)}')
+
+
+profile1 = SocialMediaProfile("johndoe")
+profile1.add_post('Hello, world!')
+profile1.add_post('Had a great day at the park!')
+profile1.add_post("What's up, Natalie? How are you?")
+profile1.dislay_timeline()
+
+# def main()
+#     pass
--- 11-ObjectOrientedProgramming/student.py
+++ 11-ObjectOrientedProgramming/student.py
@@ -3,20 +3,32 @@ class Student():
     def __init__(self):
         self.name = ""
         self.age = 0
+        self.ID = 0
 
 def main():
     # object creation based on the class
     student1 = Student()
     student2 = Student()
+    student3 = Student()
+    
     student1.name = "Dominic"
     student1.age = 19
+    student1.ID = 5672325
+
     student2.name = "Olivia"
     student2.age = 21
+    student2.ID = 235775
+
+    student3.name = "Sofia"
+    student3.age = 26
+    student3.ID = 277775
+
 
     print('LIST OF STUDENTS')
     print('================')
-    print(f'{student1.name}, {student1.age} years old')
-    print(f'{student2.name}, {student2.age} years old')
+    print(f'{student1.name}, {student1.age} years old, ID: {student1.ID}')
+    print(f'{student2.name}, {student2.age} years old, ID: {student2.ID}')
+    print(f'{student3.name}, {student3.age} years old, ID: {student3.ID}')
 
 if __name__ == "__main__":
     main()
